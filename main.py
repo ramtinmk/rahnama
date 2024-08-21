0e3d3b8b13cce535a56a6c80b5dea1e3766f7bb1
ramtinmk <ramtinmk1382@gmail.com>
2024-08-21 16:20:55 +0330

    adding database utils

--- main.py
+++ main.py
@@ -3,52 +3,29 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 import os
 import json
-import sqlite3
-from flask import g
 import logging
-
-
-# logging.basicConfig(filename='record.log', level=logging.WARNING)
+from database_utils import *
+from flask import g
 
 app = Flask(__name__)
 
 
-
-
-
-app.secret_key = secrets.token_hex(16)
-
-DATABASE = 'data/database.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    db.row_factory = make_dicts
-
-    return db
 
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
 def init_db():
     with app.app_context():
         db = get_db()
         with app.open_resource('data/schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-def make_dicts(cursor, row):
-    return dict((cursor.description[idx][0], value)
-                for idx, value in enumerate(row))
+
+
+app.secret_key = secrets.token_hex(16)
+
 
 @app.route("/")
 @app.route("/home")
@@ -169,11 +146,11 @@ def save_post():
         
         for tag in tags:
             tag_id = query_db("select tag_id from Tags where tag_name = ?",[tag],one=True)["tag_id"]
-            cursor.execute("INSERT INTO PostTags (post_id,tag_id) VALUES (?,?);"(post_id,tag_id))
+            cursor.execute("INSERT INTO PostTags (post_id,tag_id) VALUES (?,?);",(post_id,tag_id))
     except sqlite3.Error as e:
         app.logger.error(e)
         flash(e)
-        
+
     redirecting_url = url_for("/posts",post_id=post_id)
 
     return redirect(redirecting_url)
