4912d6272f90f54ecf05c179d2cbba4f8f5fd61a
mobinapn <m.paknia81@gmail.com>
2024-08-31 00:19:57 +0430

    start persian

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,13 @@
 document.querySelector('.navbar-toggler').addEventListener('click', function () {
   document.getElementById('sidebar').classList.toggle('active');
 });
+// script.js
+
+document.getElementById('sidebarToggle').addEventListener('click', function() {
+  var sidebar = document.getElementById('sidebar');
+  if (sidebar.style.display === "none" || !sidebar.style.display) {
+      sidebar.style.display = "block";
+  } else {
+      sidebar.style.display = "none";
+  }
+});
